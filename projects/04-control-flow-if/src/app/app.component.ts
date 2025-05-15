23c9a3d2c539e8b73fbfd88dbe48004a367d5ceb
Thi Pham <thipham1123@gmail.com>
2025-05-15 23:12:16 +0700

    🌱150525: lesson 4

--- projects/04-control-flow-if/src/app/app.component.ts
+++ projects/04-control-flow-if/src/app/app.component.ts
@@ -11,16 +11,30 @@ import { AccountInfo } from './account-info';
       <section class="membership-info">
         <p>
           <!-- name -->
-          {{ '' }}
+          {{ account.name }}
         </p>
-        <p>Valid Thru: {{ '' }}</p>
-        <p>CVV: {{ '' }}</p>
+        <p>Valid Thru: {{ account.validThru }}</p>
+        <p>CVV: {{ account.CVV }}</p>
+        <!-- membership status -->
         <p>
-          <!-- membership status -->
+          @if (account.membershipStatus === 'gold') {
           <span class="badge gold">Gold</span>
-          <span class="badge platinum">Platinum</span>
+          }@else if (account.membershipStatus === 'silver' ) {
           <span class="badge silver">Silver</span>
+          }@else {
+          <span class="badge platinum">Platinum</span>
+          }
         </p>
+        <!-- another method -->
+        <!-- <p>
+          @switch (account.membershipStatus) { @case ('platinum') {
+          <span class="badge platinum">Platinum</span>
+          } @case('gold') {
+          <span class="badge gold">Gold</span>
+          } @default {
+          <span class="badge silver">Silver</span>
+          } }
+        </p> -->
       </section>
     </article>
   `,
